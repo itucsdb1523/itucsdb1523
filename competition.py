bd2c53ad7150f4bdd3a41a1998cd980138950ae5
cengizmar <cengizmeh@itu.edu.tr>
2015-11-03 14:50:11 +0200

    arif degisiklik

---
+++ competition.py
@@ -0,0 +1,7 @@
+class Competition:
+    def __init__(self, id, comp_name, comp_type, comp_year, country_id):
+        self.id=id
+        self.comp_name=comp_name
+        self.comp_type=comp_type
+        self.comp_year=comp_year
+        self.country_id=country_id
